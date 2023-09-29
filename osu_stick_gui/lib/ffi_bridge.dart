e90ff50e8530d775a972298d3e33421e55d8bf62
H2Sxxa <h2sxxa0w0@gmail.com>
2023-09-29 20:21:38 +0800

    🧐 wtf the cargo ndk?

--- osu_stick_gui/lib/ffi_bridge.dart
+++ osu_stick_gui/lib/ffi_bridge.dart
@@ -8,35 +8,16 @@ import 'dart:io';
 import 'package:ffi/ffi.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:osu_stick_gui/main.dart';
 import 'package:path_provider/path_provider.dart';
 
 //final LIB_OSU_STICKER = DynamicLibrary.open("libosu_sticker.so");
 final DynamicLibrary nativeLib = handle_dylib();
 
 DynamicLibrary handle_dylib() {
-  if (Platform.isAndroid) {
-    //return DynamicLibrary.open("libosu_sticker.so");
-    String sopth = "";
-    getApplicationSupportDirectory()
-        .then((value) => sopth = "${value.path}/libosu_sticker.so");
-    rootBundle
-        .load("solib/lib/arm64-v8a/libosu_sticker.so")
-        .then((value) async {
-      Directory? external_dir = await getExternalStorageDirectory();
-      Directory inner_dir = await getApplicationSupportDirectory();
-      external_dir ??= inner_dir;
-      String dbg_so = "${external_dir.path}/dbg_libosu_sticker.so";
-      if (await File(dbg_so).exists()) {
-        logger.w("load dbg .so!");
-        await File(dbg_so).copy(sopth);
-      } else {
-        await File(sopth).writeAsBytes(value.buffer.asUint8List());
-      }
-    });
-    return DynamicLibrary.open(sopth);
-  } else if (Platform.isWindows) {
+  if (Platform.isWindows) {
     return DynamicLibrary.open('libosu_sticker.dll');
+  } else if (Platform.isAndroid) {
+    return DynamicLibrary.open("libosu_sticker.so");
   } else {
     return DynamicLibrary.process();
   }
