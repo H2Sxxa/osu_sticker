2a4d7494292cf8e3a1a36c03b56987bfc22e2076
H2Sxxa <h2sxxa0w0@gmail.com>
2023-09-30 07:19:51 +0800

    🎨 Improve and fix

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,23 @@
 use std::ffi::{c_char, c_float, c_int, CStr};
 
-use image::{load_from_memory, Rgba};
+use image::{load_from_memory, DynamicImage, Rgba};
 use imageproc::drawing::draw_text;
 use rusttype::Font;
 
-#[no_mangle]
+const FONT_B: &[u8] = include_bytes!("../resource/Aller-Bold.ttf");
+const BG_B: &[u8] = include_bytes!("../resource/osu.png");
+
+pub fn get_font() -> Font<'static> {
+    Font::try_from_bytes(FONT_B).unwrap_or_else(|| panic!("error in load font"))
+}
+
+pub fn get_bg() -> DynamicImage {
+    load_from_memory(BG_B).unwrap_or_else(|e| panic!("{}", e))
+}
+
 pub fn generate_osu_r(text: String, x: i32, y: i32, size_x: f32, size_y: f32, savepath: String) {
-    let font = Font::try_from_bytes(include_bytes!("../resource/Aller-Bold.ttf"))
-        .unwrap_or_else(|| panic!("Font load error"));
-    let bg =
-        load_from_memory(include_bytes!("../resource/osu.png")).unwrap_or_else(|e| panic!("{}", e));
     let out = draw_text(
-        &bg,
+        &get_bg(),
         Rgba([255u8, 255u8, 255u8, 255u8]),
         x,
         y,
@@ -19,7 +25,7 @@ pub fn generate_osu_r(text: String, x: i32, y: i32, size_x: f32, size_y: f32, sa
             x: size_x,
             y: size_y,
         },
-        &font,
+        &get_font(),
         &text,
     );
     out.save_with_format(savepath.as_str(), image::ImageFormat::Png)
@@ -27,7 +33,7 @@ pub fn generate_osu_r(text: String, x: i32, y: i32, size_x: f32, size_y: f32, sa
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn generate_osu(
+pub extern "C" fn generate_osu(
     text: *const c_char,
     x: c_int,
     y: c_int,
@@ -36,11 +42,12 @@ pub unsafe extern "C" fn generate_osu(
     savepath: *const c_char,
 ) {
     generate_osu_r(
-        CStr::from_ptr(text).to_string_lossy().to_string(),
+        unsafe { CStr::from_ptr(text).to_string_lossy().to_string() },
         x,
         y,
         size_x,
         size_y,
-        CStr::from_ptr(savepath).to_string_lossy().to_string(),
+        unsafe { CStr::from_ptr(savepath).to_string_lossy().to_string() },
     )
 }
+
